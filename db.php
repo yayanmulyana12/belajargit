85f2bd86bc9ef269633a314e7ef824f70fc87e2a
yayanmulyana12 <2310631170057@student.unsika.ac.id>
2025-04-17 11:31:54 +0700

    Koneksi Database

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$username = "root";
+$password = "";
+$dbname = "photo_db";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
